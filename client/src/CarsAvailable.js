d7b04656736bd835a726f4cd717fb51ce71bd5db
Copernicus <copernicus@CopernicussMini.hitronhub.home>
2023-05-06 19:30:26 -0700

    another test

--- client/src/CarsAvailable.js
+++ client/src/CarsAvailable.js
@@ -1,74 +1,92 @@
-import React from 'react'
-
+import React from "react";
 
 export default function CarsAvailable() {
   return (
     <div className="carsAvailable">
-    <div className="availableTitle"> Cars Available now</div>
+      <div className="availableTitle"> Cars Available now</div>
       <div className="availableCars">
-           <div className="carItem">
-            <div className="carName"> Car 1</div>
-            <div className="carImage"> image</div>
-            <div className="carArea">
-              <div>area</div>
-              <div className="reserve"> <a href="#">Reserve</a></div>
+        <div className="carItem">
+          <div className="carName"> Car 1</div>
+          <div className="carImage"> image</div>
+          <div className="carArea">
+            <div>area</div>
+            <div className="reserve">
+              {" "}
+              <a href="#">Reserve</a>
             </div>
-
-           </div>
-           <div className="carItem">
-            <div className="carName"> Car 2</div>
-            <div className="carImage"> image</div>
-            <div className="carArea">
-              <div>area</div>
-              <div className="reserve"> <a href="#">Reserve</a></div>
+          </div>
+        </div>
+        <div className="carItem">
+          <div className="carName"> Car 2</div>
+          <div className="carImage"> image</div>
+          <div className="carArea">
+            <div>area</div>
+            <div className="reserve">
+              {" "}
+              <a href="#">Reserve</a>
             </div>
-
-           </div>
-           <div className="carItem">
-            <div className="carName"> Car 3</div>
-            <div className="carImage"> image</div>
-            <div className="carArea">
-              <div>area</div>
-              <div className="reserve"> <a href="#">Reserve</a></div>
+          </div>
+        </div>
+        <div className="carItem">
+          <div className="carName"> Car 3</div>
+          <div className="carImage"> image</div>
+          <div className="carArea">
+            <div>area</div>
+            <div className="reserve">
+              {" "}
+              <a href="#">Reserve</a>
             </div>
-
-           </div>
-           <div className="carItem">
-            <div className="carName"> Car 4</div>
-            <div className="carImage"> image</div>
-            <div className="carArea">
-              <div>area</div>
-              <div className="reserve"> <a href="#">Reserve</a></div>
+          </div>
+        </div>
+        <div className="carItem">
+          <div className="carName"> Car 4</div>
+          <div className="carImage"> image</div>
+          <div className="carArea">
+            <div>area</div>
+            <div className="reserve">
+              {" "}
+              <a href="#">Reserve</a>
             </div>
-
-           </div>
-           <div className="carItem">
-            <div className="carName"> Car 5</div>
-            <div className="carImage"> image</div>
-            <div className="carArea">
-              <div>area</div>
-              <div className="reserve"> <a href="#">Reserve</a></div>
+          </div>
+        </div>
+        <div className="carItem">
+          <div className="carName"> Car 5</div>
+          <div className="carImage"> image</div>
+          <div className="carArea">
+            <div>area</div>
+            <div className="reserve">
+              {" "}
+              <a href="#">Reserve</a>
             </div>
-
-           </div>
-           <div className="carItem">
-            <div className="carName"> Car 6 </div>
-            <div className="carImage"> image</div>
-            <div className="carArea">
-              <div>area</div>
-              <div className="reserve"> <a href="#">Reserve</a></div>
+          </div>
+        </div>
+        <div className="carItem">
+          <div className="carName"> Car 6 </div>
+          <div className="carImage"> image</div>
+          <div className="carArea">
+            <div>area</div>
+            <div className="reserve">
+              {" "}
+              <a href="#">Reserve</a>
             </div>
-           </div>
-           <div className="carItem">
-            <div className="carName"> Car 7 </div>
-            <div className="carImage"> image</div>
-            <div className="carArea">
-              <div>area</div>
-              <div className="reserve"> <a href="#">Reserve</a></div>
+          </div>
+        </div>
+        <div className="carItem">
+          <div className="carName"> Car 7 </div>
+          <div className="carImage"> image</div>
+          <div className="carArea">
+            <div>area</div>
+            <div className="reserve">
+              {" "}
+              <a href="#">Reserve</a>
             </div>
-           </div>
-           <div className="seeMore"> <a href="#">See more</a></div>
+          </div>
+        </div>
+        <div className="seeMore">
+          {" "}
+          <a href="#">See more</a>
+        </div>
       </div>
-      </div>
-  )
+    </div>
+  );
 }
