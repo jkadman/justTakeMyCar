c140746fb84f8b9b70708aca6f55c25fdc10f81b
Copernicus <copernicus@CopernicussMini.hitronhub.home>
2023-05-09 22:22:44 -0700

    adding about.css import

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,17 @@
-import CarsAvailableseemore from './CarsAvailableseemore'
-import Header from './Header';
-import { Route, Routes} from 'react-router-dom'
-import CarsAvailable from './CarsAvailable';
-import CarsTotal from './CarsTotal';
-import Footer from './Footer';
-import Loginpage from './Loginpage';
-import Registerpage from './Registerpage'
-import StolenCar from './StolenCar'
-import About from './About'
-import index from './index'
-import './App.css';
-import Totalavailableseemore from './Totalavailableseemore';
-
-
-
+import CarsAvailableseemore from "./CarsAvailableseemore";
+import Header from "./Header";
+import { Route, Routes } from "react-router-dom";
+import CarsAvailable from "./CarsAvailable";
+import CarsTotal from "./CarsTotal";
+import Footer from "./Footer";
+import Loginpage from "./Loginpage";
+import Registerpage from "./Registerpage";
+import StolenCar from "./StolenCar";
+import About from "./About";
+import "./About.css";
+import index from "./index";
+import "./App.css";
+import Totalavailableseemore from "./Totalavailableseemore";
 
 function App() {
   return (
