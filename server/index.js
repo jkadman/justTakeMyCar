7182b4301dae8636b114af22a688f1c382102bc0
Jason Kadman <jkadman231@gmail.com>
2023-05-11 10:39:12 -0700

    add middleware

--- server/index.js
+++ server/index.js
@@ -1,5 +1,10 @@
 import express from 'express';
+import cors from 'cors';
 const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
 
 app.listen(5000, () => {
   console.log('server is listening on port 5000');
