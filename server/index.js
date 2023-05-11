74aa3cca7d2081e123ae3635a23512363fb36e56
Jason Kadman <jkadman231@gmail.com>
2023-05-11 11:08:04 -0700

    add test post request

--- server/index.js
+++ server/index.js
@@ -1,10 +1,19 @@
-import express from 'express';
-import cors from 'cors';
+const express = require('express');
+const cors = require('cors');
 const app = express();
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+app.post("/test", async(req, res) => {
+  try {
+  console.log(req.body);
+  } catch (err) {
+  console.log(err.message);
+  }
+})
+
 
 app.listen(5000, () => {
   console.log('server is listening on port 5000');
