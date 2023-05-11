2c2afa10a721c2f91a79a14037faf47974dec5a6
Jason Kadman <jkadman231@gmail.com>
2023-05-11 11:22:19 -0700

    add basic postgres commands for post request

--- server/index.js
+++ server/index.js
@@ -6,13 +6,22 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.post("/test", async(req, res) => {
+app.post("/", async(req, res) => {
   try {
   console.log(req.body);
+  // const { image, type, make, name, year } = req.body
+  // const newCar = await pool.query(
+    //"INSERT INTO <tablename> (image, type, make, name, year) VALUES($1, $2, $3, $4, $5) RETURNING * ",
+    //[image, type, make, name, year]
+  //)
   } catch (err) {
   console.log(err.message);
   }
 })
+
+// get cars
+
+app.get("/")
 
 
 app.listen(5000, () => {
