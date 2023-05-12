3cde1e2f714bfaa32810476046bd620050ecb714
Copernicus <copernicus@CopernicussMini.hitronhub.home>
2023-05-12 16:34:13 -0700

    with schema folder and each table table in different file and seeds folder with users seeds

---
+++ server/src/db/seeds/01_users.sql
@@ -0,0 +1,6 @@
+-- \i seeds/01_seeds.sql
+INSERT INTO users (first_name, last_name, phone_number, email, location, owner, drivers_licence, username, password)
+VALUES ('Aaron', 'Aaronson', '604 555 5656' 'aaaa@gmail.com', 'New Westminster', true,  'ABCD1', 'aaron', '12345'),
+('Betty', 'Blueberry', 'betblue@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
+('Colin Collins', 'collins@outlook.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u');
+
