e858c7531864111a51827e55187648b6c1ecdc87
Archit Purohit'git config --global user.name Archit <Architpurohit@hotmail.com>
2023-05-13 15:28:50 -0700

    changes in user input forms

--- client/src/Registerform.js
+++ client/src/Registerform.js
@@ -2,11 +2,12 @@ import React, { useState } from 'react';
 
 export default function SignUpForm() {
   const [formData, setFormData] = useState({
-    username: '',
+    phoneNumber: '', // Add phoneNumber field
     fullName: '',
     email: '',
     password: ''
   });
+  console.log(formData)
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -25,54 +26,55 @@ export default function SignUpForm() {
   };
 
   return (
-    <div style={{ width: '400px', height: '400px', border: '1px solid black', borderRadius: '10px', padding: '20px', margin: '0 auto' }}>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="fullName">Full Name:</label>
-        <input
-          type="text"
-          id="fullName"
-          name="fullName"
-          placeholder="Enter your full name"
-          value={formData.fullName}
-          onChange={handleInputChange}
-          required
-        />
+    <div className="container" style={{ width: '400px', height: '400px', border: '1px solid black', borderRadius: '10px', padding: '20px', margin: '0 auto' }}>
+  <form onSubmit={handleSubmit}>
+    <label htmlFor="fullName">Full Name:</label>
+    <input
+      type="text"
+      id="fullName"
+      name="fullName"
+      placeholder="Enter your full name"
+      value={formData.fullName}
+      onChange={handleInputChange}
+      required
+    />
 
-        <label htmlFor="username">Username:</label>
-        <input
-          type="text"
-          id="username"
-          name="username"
-          placeholder="Enter your username"
-          value={formData.username}
-          onChange={handleInputChange}
-          required
-        />
+    <label htmlFor="phoneNumber">Phone Number:</label>
+    <input
+      type="text"
+      id="phoneNumber"
+      name="phoneNumber"
+      placeholder="Enter your phone number"
+      value={formData.phoneNumber}
+      onChange={handleInputChange}
+      required
+    />
 
-        <label htmlFor="email">Email:</label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          placeholder="Enter your email"
-          value={formData.email}
-          onChange={handleInputChange}
-          required
-        />
+    <label htmlFor="email">Email:</label>
+    <input
+      type="email"
+      id="email"
+      name="email"
+      placeholder="Enter your email"
+      value={formData.email}
+      onChange={handleInputChange}
+      required
+    />
 
-        <label htmlFor="password">Password:</label>
-        <input
-          type="password"
-          id="password"
-          name="password"
-          placeholder="Enter your password"
-          value={formData.password}
-          onChange={handleInputChange}
-          required
-        />
+    <label htmlFor="password">Password:</label>
+    <input
+      type="password"
+      id="password"
+      name="password"
+      placeholder="Enter your password"
+      value={formData.password}
+      onChange={handleInputChange}
+      required
+    />
 
-        <button type="submit">Submit</button>
-      </form>
-    </div>
+    <button type="submit">Submit</button>
+  </form>
+</div>
+
   );
 }
