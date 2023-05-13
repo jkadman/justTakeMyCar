f618025cfb1606b2d41fb98cbff0821d58301f62
Copernicus <copernicus@CopernicussMini.hitronhub.home>
2023-05-13 14:43:35 -0700

    with public folder and car images added to that folder in server folder and cars table and seeds changed to better reference files

--- server/src/db/seeds/03_cars.sql
+++ server/src/db/seeds/03_cars.sql
@@ -1,7 +1,7 @@
 INSERT INTO cars (user_id, car_photo, make, type, name, colour, price_per_day, year)
-VALUES (1, bytea('/justTakeMyCar/client/src/data/pictures/car1.png'), 'BMW', 'sedan', 'Madame Rouge', 'red', 340.50, 2016),
-(2, bytea('/justTakeMyCar/client/src/data/pictures/car2.png'), 'Audi', 'sedan', 'Blackfly', 'black', 218.10, 2022), 
-(3, bytea('/justTakeMyCar/client/src/data/pictures/car3.png'), 'Ducati', 'motorcycle', 'Frogger', 'red', 68.10, 2020),
-(4, bytea('/justTakeMyCar/client/src/data/pictures/car4.png'), 'Audi', 'hatchback', 'Mr. Nobody', 'white', 138.50, 2018),
-(5, bytea('/justTakeMyCar/client/src/data/pictures/car5.png'), 'Jeep', 'jeep', 'Rebis', 'green', 95.99, 2014),
-(6, bytea('/justTakeMyCar/client/src/data/pictures/car6.png'), 'Bentley', 'sedan', 'Bigcoin', 'blue', 184.99, 2019);
+VALUES (1, car_photo '/pictures/car1.png', 'BMW', 'sedan', 'Madame Rouge', 'red', 340.50, 2016),
+(2, car_photo '/pictures/car2.png', 'Audi', 'sedan', 'Blackfly', 'black', 218.10, 2022), 
+(3, car_photo '/pictures/car3.png', 'Ducati', 'motorcycle', 'Frogger', 'red', 68.10, 2020),
+(4, car_photo '/pictures/car4.png', 'Audi', 'hatchback', 'Mr. Nobody', 'white', 138.50, 2018),
+(5, car_photo '/pictures/car5.png', 'Jeep', 'jeep', 'Rebis', 'green', 95.99, 2014),
+(6, car_photo '/pictures/car6.png', 'Bentley', 'sedan', 'Bigcoin', 'blue', 184.99, 2019);
