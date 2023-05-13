f28d3e7a8d23cde6ee23a856cacb33cd6b3f3e20
Copernicus <copernicus@CopernicussMini.hitronhub.home>
2023-05-12 17:03:51 -0700

    user seeds with 12 users

--- server/src/db/seeds/01_users.sql
+++ server/src/db/seeds/01_users.sql
@@ -1,6 +1,38 @@
--- \i seeds/01_seeds.sql
 INSERT INTO users (first_name, last_name, phone_number, email, location, owner, drivers_licence, username, password)
-VALUES ('Aaron', 'Aaronson', '604 555 5656' 'aaaa@gmail.com', 'New Westminster', true,  'ABCD1', 'aaron', '12345'),
-('Betty', 'Blueberry', 'betblue@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
-('Colin Collins', 'collins@outlook.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u');
+VALUES ('Aaron', 'Aaronson', '604 555 5656' 'aaaa@gmail.com', 'New Westminster', true,  'ABCD1', 'aaron', '12345');
+
+INSERT INTO users (first_name, last_name, phone_number, email, location, owner, drivers_licence, username, password)
+VALUES ('Betty', 'Blueberry', '604 111 1234', 'betblue@gmail.com', 'Vancouver', false, 'ABCD2', 'betty', '23456');
+
+INSERT INTO users (first_name, last_name, phone_number, email, location, owner, drivers_licence, username, password)
+VALUES ('Colin', 'Collins', '604 112 2234', 'collins@outlook.com', 'Langley', true, 'ABCD4', 'collin' '34567');
+
+INSERT INTO users (first_name, last_name, phone_number, email, location, owner, drivers_licence, username, password)
+VALUES ('Leslie', 'Lincod', '604 152 2334', 'leslie@outlook.com', 'Coquitlam', true, 'ABCD5', 'leslie' '45678');
+
+INSERT INTO users (first_name, last_name, phone_number, email, location, owner, drivers_licence, username, password)
+VALUES ('Jeffery', 'Ming', '604 222 2638', 'jeffery@gmail.com', 'North Vancouver', false, 'ABCD6', 'jeffery' '56789');
+
+INSERT INTO users (first_name, last_name, phone_number, email, location, owner, drivers_licence, username, password)
+VALUES ('Francis', 'Bacon', '604 894 6698', 'francis@gmail.com', 'Vancouver', false, 'ABCD7', 'francis' '56781');
+
+INSERT INTO users (first_name, last_name, phone_number, email, location, owner, drivers_licence, username, password)
+VALUES ('Thomas', 'Jefferson', '604 878 1365', 'tom@hotmail.com', 'Vancouver', false, 'ABCD8', 'tom' '98765');
+
+INSERT INTO users (first_name, last_name, phone_number, email, location, owner, drivers_licence, username, password)
+VALUES ('Ada', 'Lovelace', '604 477 2685', 'ada@outlook.com', 'Vancouver', true, 'ABCD9', 'ada' '87654');
+
+INSERT INTO users (first_name, last_name, phone_number, email, location, owner, drivers_licence, username, password)
+VALUES ('Queenie', 'Mathers', '604 589 4563', 'quennie@hotmail.com', 'Burnaby', true, 'ABCD9', 'queenie' '76543');
+
+INSERT INTO users (first_name, last_name, phone_number, email, location, owner, drivers_licence, username, password)
+VALUES ('Ching', 'Shih', '604 985 1982', 'ching@yahoo.com', 'Vancouver', false, 'ABCDE1', 'ching' '65432');
+
+INSERT INTO users (first_name, last_name, phone_number, email, location, owner, drivers_licence, username, password)
+VALUES ('Oscar', 'Peterson', '604 333 9145', 'oscar@hotmail.com', 'Vancouver', true, 'ABCDE2', 'oscar' '54321');
+
+INSERT INTO users (first_name, last_name, phone_number, email, location, owner, drivers_licence, username, password)
+VALUES ('Maya', 'Angelou', '604 135 7344', 'maya@gmail.com', 'Vancouver', false, 'ABCDE3', 'maya' '43210');
+
+
 
