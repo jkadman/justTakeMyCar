06fd18f641a3ecf39c18026b6cbac8c3831050e5
Jason Kadman <jkadman231@gmail.com>
2023-05-16 14:45:33 -0700

    experiment with ideas for refactoring

--- server/src/db/db.js
+++ server/src/db/db.js
@@ -5,7 +5,8 @@ const config = {
   user: process.env.PGUSER,
   password: process.env.PGPASSWORD,
   host: process.env.PGHOST,
-  database: process.env.PGDATABASE
+  database: process.env.PGDATABASE,
+  port: process.env.PGPORT
 }
 
 const pool = new Pool(config)
