4ceff6348627095287d0a466931bb739f801df30
Copernicus <copernicus@CopernicussMini.hitronhub.home>
2023-05-15 18:48:21 -0700

    register car setup and can now see all the cars on carsAvailable and carsTotal page, also changed index.js and registercar

--- client/src/CarsAvailable.js
+++ client/src/CarsAvailable.js
@@ -1,5 +1,4 @@
-import React, { Fragment, useEffect, useState } from "react";
-import carData from "./data/carData";
+import React, { useEffect, useState } from "react";
 import useNavigation from "./hooks/navigate";
 import Reservepage from "./Reservepage";
 import { useNavigate } from "react-router-dom";
@@ -23,25 +22,26 @@ export default function CarsAvailable() {
     getCars();
   }, []);
 
-  const carList = cars.map((car) => {
+  const carListA = cars.map((car, index) => {
     return (
-      <div key={car.id} className="carItem">
-        <div className="carName">
-          {car.year} {car.make} {car.name}
-        </div>
-        <div className="carImage">
-          <img src={car.car_photo} alt="car1"></img>
-        </div>
-        <div className="carArea">
-          <div>area</div>
-          <div className="reserve">
-            {" "}
-            <a className="Reserve" onClick={() => navigateTo("/Reserve")}>
-              Reserve
-            </a>
+      index < 7 && (
+        <div key={car.id} className="carItem">
+          <div className="carName">
+            {car.year} {car.make} {car.name}
+          </div>
+          <div className="carImage">
+            <img src={car.car_photo} alt="car1"></img>
+          </div>
+          <div className="carArea">
+            <div>area</div>
+            <div className="reserve">
+              <a className="Reserve" onClick={() => navigateTo("/Reserve")}>
+                Reserve
+              </a>
+            </div>
           </div>
         </div>
-      </div>
+      )
     );
   });
 
@@ -49,114 +49,8 @@ export default function CarsAvailable() {
     <div className="carsAvailable">
       <div className="availableTitle"> Cars Available now</div>
       <div className="availableCars">
-        {carList}
-        {/* <div className="carItem">
-          <div key={cars.year} className="carName">
-            {cars[0].year} {carData[0].make} {carData[0].name}
-          </div>
-          <div className="carImage">
-            <img src={carData[0].image} alt="car1"></img>
-          </div>
-          <div className="carArea">
-            <div>area</div>
-            <div className="reserve">
-              {" "}
-              <a className="Reserve" onClick={() => navigateTo("/Reserve")}>
-                Reserve
-              </a>
-            </div>
-          </div>
-        </div> */}
-        {/* <div className="carItem">
-          <div className="carName"></div>
-          <div className="carImage">
-            <img src={carData[1].image} alt="car2"></img>
-          </div>
-          <div className="carArea">
-            <div>area</div>
-            <div className="reserve">
-              {" "}
-              <a href="#">Reserve</a>
-            </div>
-          </div>
-        </div> */}
-        {/* <div className="carItem">
-          <div className="carName">
-            {carData[2].year} {carData[2].make} {carData[2].name}
-          </div>
-          <div className="carImage">
-            <img src={carData[2].image} alt="car3"></img>
-          </div>
-          <div className="carArea">
-            <div>area</div>
-            <div className="reserve">
-              {" "}
-              <a href="#">Reserve</a>
-            </div>
-          </div>
-        </div>
-        <div className="carItem">
-          <div className="carName">
-            {carData[3].year} {carData[3].make} {carData[3].name}
-          </div>
-          <div className="carImage">
-            <img src={carData[3].image} alt="car4"></img>
-          </div>
-          <div className="carArea">
-            <div>area</div>
-            <div className="reserve">
-              {" "}
-              <a href="#">Reserve</a>
-            </div>
-          </div>
-        </div>
-        <div className="carItem">
-          <div className="carName">
-            {carData[4].year} {carData[4].make} {carData[4].name}
-          </div>
-          <div className="carImage">
-            <img src={carData[4].image} alt="car5"></img>
-          </div>
-          <div className="carArea">
-            <div>area</div>
-            <div className="reserve">
-              {" "}
-              <a href="#">Reserve</a>
-            </div>
-          </div>
-        </div>
-        <div className="carItem">
-          <div className="carName">
-            {carData[5].year} {carData[5].make} {carData[5].name}
-          </div>
-          <div className="carImage">
-            <img src={carData[5].image} alt="car6"></img>
-          </div>
-          <div className="carArea">
-            <div>area</div>
-            <div className="reserve">
-              {" "}
-              <a href="#">Reserve</a>
-            </div>
-          </div>
-        </div>
-        <div className="carItem">
-          <div className="carName">
-            {carData[6].year} {carData[6].make} {carData[6].name}
-          </div>
-          <div className="carImage">
-            <img src={carData[6].image} alt="car7"></img>
-          </div>
-          <div className="carArea">
-            <div>area</div>
-            <div className="reserve">
-              {" "}
-              <a href="#">Reserve</a>
-            </div>
-          </div>
-        </div> */}
+        {carListA}
         <div className="seeMore">
-          {" "}
           <button
             className="seeMore"
             onClick={() => navigateTo("/Carsavailablemore")}
--- client/src/CarsTotal.js
+++ client/src/CarsTotal.js
@@ -1,120 +1,57 @@
-import React from "react";
-import carData from "./data/carData";
+import React, { useEffect, useState } from "react";
 import useNavigation from "./hooks/navigate";
+import Reservepage from "./Reservepage";
+import { useNavigate } from "react-router-dom";
 
-export default function CarsTotal() {
+export default function CarsAvailable() {
+  const navigate = useNavigate();
   const { navigateTo } = useNavigation();
+  const [cars, setCars] = useState([]);
+  const getCars = async () => {
+    try {
+      const response = await fetch("http://localhost:5001");
+      const jsonData = await response.json();
+      //console.log(jsonData);
+      setCars(jsonData);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    getCars();
+  }, []);
+
+  const carListB = cars.map((car, index) => {
+    return (
+      index < 15 &&
+      index > 7 && (
+        <div key={car.id} className="carItem">
+          <div className="carName">
+            {car.year} {car.make} {car.name}
+          </div>
+          <div className="carImage">
+            <img src={car.car_photo} alt="car1"></img>
+          </div>
+          <div className="carArea">
+            <div>area</div>
+            <div className="reserve">
+              <a className="Reserve" onClick={() => navigateTo("/Reserve")}>
+                Reserve
+              </a>
+            </div>
+          </div>
+        </div>
+      )
+    );
+  });
+
   return (
     <div>
       <div className="totalTitle"> Cars in stock</div>
       <div className="registeredCars">
-        <div className="carItem">
-          <div className="carName">
-            {carData[7].year} {carData[7].make} {carData[7].name}
-          </div>
-          <div className="carImage">
-            <img src={carData[7].image} alt="car8"></img>
-          </div>
-          <div className="carArea">
-            <div>area</div>
-            <div className="reserve">
-              {" "}
-              <a href="#">Reserve</a>
-            </div>
-          </div>
-        </div>
-        <div className="carItem">
-          <div className="carName">
-            {carData[8].year} {carData[8].make} {carData[8].name}
-          </div>
-          <div className="carImage">
-            <img src={carData[8].image} alt="car9"></img>
-          </div>
-          <div className="carArea">
-            <div>area</div>
-            <div className="reserve">
-              {" "}
-              <a href="#">Reserve</a>
-            </div>
-          </div>
-        </div>
-        <div className="carItem">
-          <div className="carName">
-            {carData[9].year} {carData[9].make} {carData[9].name}
-          </div>
-          <div className="carImage">
-            <img src={carData[9].image} alt="car10"></img>
-          </div>
-          <div className="carArea">
-            <div>area</div>
-            <div className="reserve">
-              {" "}
-              <a href="#">Reserve</a>
-            </div>
-          </div>
-        </div>
-        <div className="carItem">
-          <div className="carName">
-            {carData[10].year} {carData[10].make} {carData[10].name}
-          </div>
-          <div className="carImage">
-            <img src={carData[10].image} alt="car11"></img>
-          </div>
-          <div className="carArea">
-            <div>area</div>
-            <div className="reserve">
-              {" "}
-              <a href="#">Reserve</a>
-            </div>
-          </div>
-        </div>
-        <div className="carItem">
-          <div className="carName">
-            {carData[11].year} {carData[11].make} {carData[11].name}
-          </div>
-          <div className="carImage">
-            <img src={carData[11].image} alt="car12"></img>
-          </div>
-          <div className="carArea">
-            <div>area</div>
-            <div className="reserve">
-              {" "}
-              <a href="#">Reserve</a>
-            </div>
-          </div>
-        </div>
-        <div className="carItem">
-          <div className="carName">
-            {carData[12].year} {carData[12].make} {carData[12].name}
-          </div>
-          <div className="carImage">
-            <img src={carData[12].image} alt="car13"></img>
-          </div>
-          <div className="carArea">
-            <div>area</div>
-            <div className="reserve">
-              {" "}
-              <a href="#">Reserve</a>
-            </div>
-          </div>
-        </div>
-        <div className="carItem">
-          <div className="carName">
-            {carData[13].year} {carData[13].make} {carData[13].name}
-          </div>
-          <div className="carImage">
-            <img src={carData[13].image} alt="car14"></img>
-          </div>
-          <div className="carArea">
-            <div>area</div>
-            <div className="reserve">
-              {" "}
-              <a href="#">Reserve</a>
-            </div>
-          </div>
-        </div>
+        {carListB}
         <div className="seeMore">
-          {" "}
           <button
             className="seeMore"
             onClick={() => navigateTo("/Totalavailableseemore")}
@@ -122,6 +59,7 @@ export default function CarsTotal() {
             See More
           </button>
         </div>
+        ;
       </div>
     </div>
   );
