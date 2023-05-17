97c5d48a3f758cd3d7bc53319073c21db43207ec
Copernicus <copernicus@CopernicussMini.hitronhub.home>
2023-05-16 20:43:19 -0700

    remove changes while talking to Juliana

--- client/src/Reservepage.js
+++ client/src/Reservepage.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
-import './Reservepage.css';
-import 'react-datepicker/dist/react-datepicker.css';
-import DatePicker from 'react-datepicker';
+import React, { useState } from "react";
+import "./Reservepage.css";
+import "react-datepicker/dist/react-datepicker.css";
+import DatePicker from "react-datepicker";
 
 export default function Reservepage() {
   const [startDate, setStartDate] = useState(null);
@@ -17,29 +17,29 @@ export default function Reservepage() {
   };
 
   return (
-    <div id='container1'>
-      <div id='header1'>
-        <div className='header12'>Car name</div>
-        <div className='header123'>Username</div>
+    <div id="container1">
+      <div id="header1">
+        <div className="header12">Car name</div>
+        <div className="header123">Username</div>
       </div>
 
-      <div id='image'></div>
+      <div id="image"></div>
 
-      <div id='footer1'>
-        <div className='item1'>
+      <div id="footer1">
+        <div className="item1">
           <h4>Location</h4>
         </div>
-        <div className='item1'>
+        <div className="item1">
           <h4>Price</h4>
         </div>
-        <div className='datePicker'>
+        <div className="datePicker">
           <DatePicker
             selected={startDate}
             onChange={(date) => setStartDate(date)}
             selectsStart
             startDate={startDate}
             endDate={endDate}
-            placeholderText='Start Date'
+            placeholderText="Start Date"
           />
           <DatePicker
             selected={endDate}
@@ -48,21 +48,26 @@ export default function Reservepage() {
             startDate={startDate}
             endDate={endDate}
             minDate={startDate}
-            placeholderText='End Date'
+            placeholderText="End Date"
           />
         </div>
 
-        <button id='reserve' className='datePicker' onClick={handleReserveClick}>
+        <button
+          id="reserve"
+          className="datePicker"
+          onClick={handleReserveClick}
+        >
           Reserve
         </button>
       </div>
 
-
       {showPopup && (
-        <div className='popup'>
-          <div className='popup-content'>
+        <div className="popup">
+          <div className="popup-content">
             <h1>Reservation Confirmation</h1>
-            <p>Your reservation request has been sent to the owner of the car.</p>
+            <p>
+              Your reservation request has been sent to the owner of the car.
+            </p>
             <button onClick={closePopup}>Close</button>
           </div>
         </div>
