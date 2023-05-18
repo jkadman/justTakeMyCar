8bdfd1dff533d4ce73d1ad688c7dbb4141684593
Jason Kadman <jkadman231@gmail.com>
2023-05-18 02:02:25 -0700

    userpage now redirects to login

--- client/src/Userpage.js
+++ client/src/Userpage.js
@@ -1,15 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Navigate } from 'react-router-dom';
 import './Userpage.css';
 
 
 export default function Userpage() {
+  const [userData, setUserData] = useState(null);
+  const [isAuthenticated, setIsAuthenticated] = useState(true);
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const token = localStorage.getItem('token');
+        const response = await axios.get('/Userpage', {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        setUserData(response.data);
+        console.log('responseData', response.data)
+      } catch (error) {
+        console.error(error);
+        setIsAuthenticated(false);
+      }
+    };
+
+    fetchUserData();
+  }, []);
+
+  if (!isAuthenticated) {
+    return <Navigate replace to ="/login" />;
+  }
 
   return (
     <div id='body'>
-      <div id='user'>
+      {userData ? (
+        <div id='user'>
         <h2>User Name</h2>
         <p>Email: user@example.com</p>
       </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+      
 
   <h2>Rented Cars</h2>
 <div id='rentedCon'>
