c28414ca7495141e0c6072b1ce5f69f0757ece0c
Jason Kadman <jkadman231@gmail.com>
2023-05-21 16:36:27 -0700

    remove key

--- client/src/googleMap.js
+++ client/src/googleMap.js
@@ -1,33 +1,29 @@
-import { GoogleMap, LoadScript } from '@react-google-maps/api';
+import { GoogleMap, useLoadScript } from '@react-google-maps/api';
+import { useMemo } from "react";
 
-const containerStyle = {
-  width: '400px',
-  height: '400px'
-};
 
-const center = {
-  lat: -3.745,
-  lng: -38.523
-};
 
-export default function map() {
-  
+export default function Map() {
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: ""
+  });
+  const center = useMemo(() => ({ lat: 18.52043, lng: 73.856743 }), []);
 
 
 
   return(
       
-    <LoadScript
-      googleMapsApiKey="AIzaSyBxZ-YzAppn9e1gnll7gJBuVrUuhT8MFJo"
-    >
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        center={center}
-        zoom={10}
-      >
-        { /* Child components, such as markers, info windows, etc. */ }
-        <></>
-      </GoogleMap>
-    </LoadScript>
+    <div className="App">
+      {!isLoaded ? (
+        <h1>Loading...</h1>
+      ) : (
+        <GoogleMap
+          mapContainerClassName="map-container"
+          center={center}
+          zoom={10}
+        />
+      )}
+      
+    </div>
 ) 
 } 
