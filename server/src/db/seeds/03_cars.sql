7158ef129b40da8402b3c13fac081556c0930097
Copernicus <copernicus@CopernicussMini.hitronhub.home>
2023-05-23 11:53:33 -0700

    we are each owners of one car

--- server/src/db/seeds/03_cars.sql
+++ server/src/db/seeds/03_cars.sql
@@ -1,8 +1,8 @@
 INSERT INTO cars (user_id, car_photo, make, type, name, colour, price_per_day, year, street)
 VALUES
-  (1, '/pictures/car1.png', 'BMW', 'sedan', 'Madame Rouge', 'red', 340.50, 2016, 'Cambie St.'),
-  (2, '/pictures/car2.png', 'Audi', 'sedan', 'Blackfly', 'black', 218.10, 2022, 'Lougheed Ave.'),
-  (3, '/pictures/car3.png', 'Ducati', 'motorcycle', 'Frogger', 'red', 68.10, 2020, 'Ontario St.'),
+  (18, '/pictures/car1.png', 'BMW', 'sedan', 'Madame Rouge', 'red', 340.50, 2016, 'Cambie St.'),
+  (16, '/pictures/car2.png', 'Audi', 'sedan', 'Blackfly', 'black', 218.10, 2022, 'Lougheed Ave.'),
+  (17, '/pictures/car3.png', 'Ducati', 'motorcycle', 'Frogger', 'red', 68.10, 2020, 'Ontario St.'),
   (4, '/pictures/car4.png', 'Audi', 'hatchback', 'Mr. Nobody', 'white', 138.50, 2018, '12th St.'),
   (5, '/pictures/car5.png', 'Jeep', 'jeep', 'Rebis', 'green', 95.99, 2014, 'Commercial St.'),
   (6, '/pictures/car6.png', 'Bentley', 'sedan', 'Bigcoin', 'blue', 184.99, 2019, 'Clarke Rd.'),
@@ -20,15 +20,9 @@ VALUES
   (6, '/pictures/car18.png', 'Toyota', 'sedan', 'Corolla', 'grey', 74.25, 2016, 'Oak St.'),
   (7, '/pictures/car19.png', 'Toyota', 'sedan', 'Pumpkin', 'orange', 88.23, 2023, 'Canada Way'),
   (8, '/pictures/car20.png', 'Bentley', 'convertible', 'Wizard', 'white', 198.99, 2020, 'Clarke Rd.'),
-  (9, '/pictures/car21.png', 'Toyota', 'hatchback', 'Dracula', 'red', 63.50, 2022, 'Cambie St.');
-
-
-INSERT INTO cars (user_id, car_photo, make, type, name, colour, price_per_day, year, street)
-VALUES (9, '/pictures/car21.png', 'Toyota', 'hatchback', 'Dracula', 'red', 63.50, 2022, 'Cambie St.');
-(10, '/pictures/car22.png', 'Volksvagen', 'sedan', 'Blanche', 'white', 28.62, 2000, 'Hastings St.'),
-(11, '/pictures/car23.png', 'Porche', 'sport', 'Velvet', 'red', 215.20, 20223, 'Oak St.'),
-(12, '/pictures/car24.png', 'Porche', 'sport convertible', 'Trickster', 'white', 155.09, 2019, '16th Avenue');
-
-INSERT INTO cars (user_id, car_photo, make, type, name, colour, price_per_day, year, street)
-VALUES (10, '/pictures/car22.png', 'Volksvagen', 'sedan', 'Blanche', 'white', 28.62, 2000, 'Hastings St.'),
+  (9, '/pictures/car21.png', 'Toyota', 'hatchback', 'Dracula', 'red', 63.50, 2022, 'Cambie St.'),
+  (10, '/pictures/car22.png', 'Volksvagen', 'sedan', 'Blanche', 'white', 28.62, 2000, 'Hastings St.'),
 (11, '/pictures/car23.png', 'Porche', 'sport', 'Velvet', 'red', 215.20, 20223, 'Oak St.');
+
+
+
